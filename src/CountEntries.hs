94cf246cfed2f68070b7eb9fec70cc732da5eedb
Louis Suo <louis.suo@myob.com>
2017-11-20 15:21:13 +1100

    demonstrate IO unit test

--- src/CountEntries.hs
+++ src/CountEntries.hs
@@ -1,21 +1,27 @@
 -- file: ch18/CountEntries.hs
-module CountEntries (listDirectory, countEntriesTrad) where
+module CountEntries (listDirectory, countEntriesTrad, MonadFile(..)) where
 
-  import System.Directory (doesDirectoryExist, getDirectoryContents)
-  import System.FilePath ((</>))
-  import Control.Monad (forM, liftM)
+-- import System.Directory (doesDirectoryExist, getDirectoryContents)
+import           Control.Monad   (forM, liftM)
+import           System.FilePath ((</>))
 
-  listDirectory :: FilePath -> IO [String]
-  listDirectory = liftM (filter notDots) . getDirectoryContents
-      where notDots p = p /= "." && p /= ".."
+-- step 1. define Monad and abstract methods
+class Monad m => MonadFile m where
+  getDirectoryContents :: FilePath -> m [String]
+  doesDirectoryExist :: FilePath -> m Bool
 
-  countEntriesTrad :: FilePath -> IO [(FilePath, Int)]
-  countEntriesTrad path = do
-    contents <- listDirectory path
-    rest <- forM contents $ \name -> do
-              let newName = path </> name
-              isDir <- doesDirectoryExist newName
-              if isDir
-                then countEntriesTrad newName
-                else return []
-    return $ (path, length contents) : concat rest
+-- step 2. use MonadFile instead of IO
+listDirectory :: (MonadFile m) => FilePath -> m [String]
+listDirectory = fmap (filter notDots) . getDirectoryContents
+    where notDots p = p /= "." && p /= ".."
+
+countEntriesTrad :: (MonadFile m) => FilePath -> m [(FilePath, Int)]
+countEntriesTrad path = do
+  contents <- listDirectory path
+  rest <- forM contents $ \name -> do
+            let newName = path </> name
+            isDir <- doesDirectoryExist newName
+            if isDir
+              then countEntriesTrad newName
+              else return []
+  return $ (path, length contents) : concat rest
